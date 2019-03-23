ec74acbccf5fda2e34f88736f0c38cecfc35f339
DetyshevSA <ruinrut@gmail.com>
2019-03-23 11:58:05 +0300

    Старт таймера и перезагрузкка по хоткею

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
+var goPlay;
+var totalSeconds;
+
 var app = (function () {
 
 	var timer;
-	var totalSeconds;
 
 	var start = function() {
 		timer = setInterval(updateClock, 1000);
 		totalSeconds = 3000;
+		goPlay = false;
 	}	
 	
 	var updateClock = function() {
-		--totalSeconds;
-	    var hour = Math.floor(totalSeconds / 3600);
+		if(goPlay == true)
+		{
+		    --totalSeconds;
+		}
+		var hour = Math.floor(totalSeconds / 3600);
 	    var minute = Math.floor((totalSeconds - hour * 3600) / 60);
 	    var seconds = totalSeconds - (hour * 3600 + minute * 60);
 
@@ -26,7 +32,7 @@ var app = (function () {
 			seconds = "0" + seconds;
 		}
 		
-		$("h1").text(minute + ":" + seconds);
+		$("div1").text(minute + ":" + seconds);
 
 		if(totalSeconds == 0)
 		{
@@ -44,7 +50,13 @@ document.onkeyup = function(e)
 { 
 	if (e.which == 77) 
 	{
-	  alert("Начало матча!");
+	  goPlay = true;
+	}
+
+	if (e.which == 66) 
+	{
+	  goPlay = false;
+	  totalSeconds = 3000;
 	}
 };
 
